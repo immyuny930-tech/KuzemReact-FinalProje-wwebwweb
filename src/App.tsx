fab4211d84def2c54f392c6d0844778404d083cc
Imraan Unal <imraanunal930@gmail.com>
2026-08-28 15:39:23 +0300

    Routelar ve sayfa importalri TarifProvider ile baglandi

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { TarifProvider } from "./context/TarifContext";
 
+import Navbar from "./components/Navbar";
+import AnaSayfa from "./sayfalar/AnaSayfa"
+import Tarifler from "./sayfalar/Tarifler"
+import TarifDetay from "./sayfalar/TarifDetay"
+import TarifEkle from "./sayfalar/TarifEkle"
+import Favoriler from "./sayfalar/Favoriler"
 
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold text-blue-600">
-        Merhaba TailWind 
-      </h1>
-    </>
+    <TarifProvider>
+      <BrowserRouter>
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={<AnaSayfa />} />
+          <Route path="/tarifler" element={<Tarifler />} />
+          <Route path="/tarif/:id" element={<TarifDetay />} />
+          <Route path="/tarif-ekle" element={<TarifEkle />} />
+          <Route path="/favoriler" element={<Favoriler />} />
+        </Routes>
+
+      </BrowserRouter>
+    </TarifProvider>
   )
 }
 
